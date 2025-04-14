028f57ded44b77bb31a5db544578e250a27f5211
mohamed abass tawana <aminabass987@gmail.com>
2025-04-14 23:14:26 +0300

    added changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 function NavBar() {
   return (
     <nav>
@@ -16,17 +17,27 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+      <p>This is the About page.</p>
+    </div>
+  );
+}
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home /> 
+      <About /> 
     </div>
   );
 }
 
 export default App;
+
+
+
